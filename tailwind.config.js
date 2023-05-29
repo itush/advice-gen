2e4a7f9c8c0d49a4e75858b815d5cf5db51e6ef2
Tushar <itusharbiswas@gmail.com>
2023-05-29 17:26:17 +0530

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        LightCyan: "hsl(193, 38%, 86%)",
+        NeonGreen: "hsl(150, 100%, 66%)",
+        GrayishBlue: "hsl(217, 19%, 38%)",
+        DarkGrayishBlue: "hsl(217, 19%, 24%)",
+        DarkBlue: "hsl(218, 23%, 16%)",          
       },
+      boxShadow: {
+        "dice-btn-shadow": "0rem 0rem 1rem #52ffa8"
+      }
     },
   },
   plugins: [],
